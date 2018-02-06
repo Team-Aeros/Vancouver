44730afd283a4db094b710ce6c81ab347d8320e2
Robert Monden <dev@robertmonden.com>
2018-02-06 12:24:13 +0100

    Commented out log code

--- src/com/aeros/main/Util.java
+++ src/com/aeros/main/Util.java
@@ -16,16 +16,16 @@ public abstract class Util {
 
     public static void throwError(String message) {
         System.out.printf("==> A fatal error occurred: %s\n", message);
-        LogEntry.create(message);
+        //LogEntry.create(message);
     }
 
     public static void throwError(String message, String detailedError) {
         System.out.printf("==> A fatal error occurred: %s\n%s\n", message, detailedError);
-        LogEntry.create(message);
+        //LogEntry.create(message);
     }
 
     public static void printStatus(String status) {
         System.out.printf("==> %s\n", status);
-        LogEntry.create(status);
+        //LogEntry.create(status);
     }
 }
